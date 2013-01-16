98223b45f3a2a99a12c132117f287986852081a0
Julien Danjou <julien@danjou.info>
2013-01-16 22:32:42 +0100

    Update to 0.2
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import setuptools
 
 setuptools.setup(
     name="pymunincli",
-    version="0.1",
+    version="0.2",
     author="Julien Danjou",
     author_email="julien@danjou.info",
     description="munin client library",
